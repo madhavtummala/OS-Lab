097a16346027dbbf67deda428dacea70fd1533b1
MadhavChoudhary <madhavtummala@icloud.com>
2018-11-20 00:15:32 +0530

    Fixed Bankers algo

--- 10/Banker.cpp
+++ 10/Banker.cpp
@@ -2,65 +2,15 @@
 
 using namespace std;
 
-void request(int *avail, int **maxm, int **alloc, int **req, int n, int m)
+bool isSafe(int *avail, int **maxm, int **allot, int **need, int n, int m) 
 {
-	int **need = new int*[n];
-	for(int i=0;i<n;i++) need[i]=new int[m];
-
-	int P=n;
-	int R=m;
-
-	//Calculating need matrix
-    for (int i = 0 ; i < P ; i++) 
-        for (int j = 0 ; j < R ; j++) 
-            need[i][j] = maxm[i][j]-alloc[i][j];
-
-    for(int i=0;i<n;i++){
-    	for(int j=0;j<m;j++)
-    		if(req[i][j]>need[i][j]){
-    			cout<<"Error, requested more than needed\n";
-    			return;
-    		}
-    }
-
-    for(int i=0;i<n;i++){
-    	int j;
-    	for(j=0;j<m;j++){
-    		if(req[i][j]>avail[j]){
-    			cout<<"Error, no availiability for process "<<i<<endl;
-    			break;
-    		}
-    	}
-    	if(j==m)
-    	{
-    		for(j=0;j<m;j++){
-    			avail[j]-=req[i][j];
-    			alloc[i][j]+=req[i][j];
-    			need[i][j]-=req[i][j];
-    		}
-    		cout<<"satisfied request of process "<<i<<endl;
-    	}
-    }
-
-}
-
-bool isSafe(int *avail, int **maxm, int **allot, int n, int m) 
-{
-	int **need = new int*[n];
-	for(int i=0;i<n;i++) need[i]=new int[m];
-
-	int P=n;
-	int R=m;
-
-	//Calculating need matrix
-    for (int i = 0 ; i < P ; i++) 
-        for (int j = 0 ; j < R ; j++) 
-            need[i][j] = maxm[i][j]-allot[i][j]; 
+    int P=n;
+    int R=m;
   
     // Mark all processes as infinish 
     bool finish[P];
     for(int i=0;i<n;i++)
-    	finish[i]=0; 
+        finish[i]=0; 
   
     // To store safe sequence 
     int safeSeq[P]; 
@@ -132,21 +82,86 @@ bool isSafe(int *avail, int **maxm, int **allot, int n, int m)
     return true; 
 } 
 
+void request(int *avail, int **maxm, int **alloc, int *req, int n, int m, int mode, int p)
+{
+	int **need = new int*[n];
+	for(int i=0;i<n;i++) need[i]=new int[m];
+
+	//Calculating need matrix
+    for (int i = 0 ; i < n ; i++) 
+        for (int j = 0 ; j < m ; j++) 
+            need[i][j] = maxm[i][j]-alloc[i][j];
+
+
+    for(int j=0;j<m;j++)
+    	if(req[j]>need[p][j]){
+    		cout<<"Error, requested more than needed\n";
+    		return;
+    	}
+
+    if(mode==0){
+        for(int j=0;j<m;j++){
+        	if(req[j]>avail[j]){
+        		cout<<"Error, no availiability for process\n";
+        		return;
+        	}
+        }
+    }
+
+
+    if(mode==0)
+    {
+        for(int j=0;j<m;j++){
+        	avail[j]-=req[j];
+        	alloc[p][j]+=req[j];
+        	need[p][j]-=req[j];
+        }
+    }
+    else
+    {
+        for(int j=0;j<m;j++){
+            avail[j]+=req[j];
+            alloc[p][j]-=req[j];
+            need[p][j]+=req[j];
+        }       
+    }
+    if(!isSafe(avail,maxm,alloc,need,n,m))
+    {
+        printf("Reverting back the request\n");
+        if(mode==0)
+        {
+            for(int j=0;j<m;j++){
+                avail[j]+=req[j];
+                alloc[p][j]-=req[j];
+                need[p][j]+=req[j];
+            }
+        }
+        else
+        {
+            for(int j=0;j<m;j++){
+                avail[j]-=req[j];
+                alloc[p][j]+=req[j];
+                need[p][j]-=req[j];
+            }
+        }
+    }
+
+
+}
+
 int main()
 {
 	int n,m;
 	cin>>n>>m;
 
 	int *availiable = new int[m];
+    int *req = new int[m];
 
 	int **max = new int*[n];
 	for(int i=0;i<n;i++) max[i]=new int[m];
 
 	int **allocation = new int*[n];
 	for(int i=0;i<n;i++) allocation[i]=new int[m];
-
-	int **req = new int*[n];
-	for(int i=0;i<n;i++) req[i]=new int[m];
 
 	cout<<"Enter availiablity matrix\n";
 	for(int i=0;i<m;i++)
@@ -157,19 +172,35 @@ int main()
 		for(int j=0;j<m;j++)
 			cin>>max[i][j];
 
-	cout<<"Enter allocation matrix\n";
-	for(int i=0;i<n;i++)
-		for(int j=0;j<m;j++)
-			cin>>allocation[i][j];
+	// cout<<"Enter allocation matrix\n";
+	// for(int i=0;i<n;i++)
+	// 	for(int j=0;j<m;j++)
+	// 		cin>>allocation[i][j];
 
-	cout<<"Enter request matrix?\n";
-	for(int i=0;i<n;i++)
-		for(int j=0;j<m;j++)
-			cin>>req[i][j];	
+    int p;
+    char mode;
+    while(1)
+    {
+        cout<<"Enter a request\n";
+        cin>>p;
+        cin>>mode;
 
-	isSafe(availiable, max, allocation, n, m);
-	request(availiable, max, allocation, req, n, m);
-	isSafe(availiable, max, allocation, n, m);
+        if(mode=='a')
+        {
+            for(int i=0;i<m;i++)
+                cin>>req[i];
+            request(availiable, max, allocation, req, n, m, 0, p);
+        }
+
+        if(mode=='r')
+        {
+            for(int i=0;i<m;i++)
+                cin>>req[i];
+            request(availiable, max, allocation, req, n, m, 1, p);
+        }
+        cout<<endl;
+
+    }
 
 	return 0;
 

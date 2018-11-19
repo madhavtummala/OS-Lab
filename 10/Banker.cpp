d5f4ebe0f1b18df8b5d41cd33f0b163c27cc394e
MadhavChoudhary <madhavtummala@icloud.com>
2018-11-19 23:29:23 +0530

    Wrote Bankers Algo

--- 10/Banker.cpp
+++ 10/Banker.cpp
@@ -1,73 +1,111 @@
-#include<iostream> 
+#include <iostream>
 
-using namespace std; 
-  
-const int P = 3;
-const int R = 1; 
-  
-void calculateNeed(int need[P][R], int maxm[P][R], int allot[P][R]) 
-{ 
+using namespace std;
 
+void request(int *avail, int **maxm, int **alloc, int **req, int n, int m)
+{
+	int **need = new int*[n];
+	for(int i=0;i<n;i++) need[i]=new int[m];
+
+	int P=n;
+	int R=m;
+
+	//Calculating need matrix
     for (int i = 0 ; i < P ; i++) 
-        for (int j = 0 ; j < R ; j++)
-            need[i][j] = maxm[i][j] - allot[i][j]; 
-} 
-  
-bool isSafe(int processes[], int avail[], int maxm[][R], 
-            int allot[][R]) 
-{ 
-    if(allot[P-1][R-1]==3) {cout << "System is not in safe state";return false;}
-    int need[P][R]; 
-  
+        for (int j = 0 ; j < R ; j++) 
+            need[i][j] = maxm[i][j]-alloc[i][j];
 
-    calculateNeed(need, maxm, allot); 
-  
+    for(int i=0;i<n;i++){
+    	for(int j=0;j<m;j++)
+    		if(req[i][j]>need[i][j]){
+    			cout<<"Error, requested more than needed\n";
+    			return;
+    		}
+    }
 
-    bool finish[P] = {0}; 
-  
+    for(int i=0;i<n;i++){
+    	int j;
+    	for(j=0;j<m;j++){
+    		if(req[i][j]>avail[j]){
+    			cout<<"Error, no availiability for process "<<i<<endl;
+    			break;
+    		}
+    	}
+    	if(j==m)
+    	{
+    		for(j=0;j<m;j++){
+    			avail[j]-=req[i][j];
+    			alloc[i][j]+=req[i][j];
+    			need[i][j]-=req[i][j];
+    		}
+    		cout<<"satisfied request of process "<<i<<endl;
+    	}
+    }
 
+}
+
+bool isSafe(int *avail, int **maxm, int **allot, int n, int m) 
+{
+	int **need = new int*[n];
+	for(int i=0;i<n;i++) need[i]=new int[m];
+
+	int P=n;
+	int R=m;
+
+	//Calculating need matrix
+    for (int i = 0 ; i < P ; i++) 
+        for (int j = 0 ; j < R ; j++) 
+            need[i][j] = maxm[i][j]-allot[i][j]; 
+  
+    // Mark all processes as infinish 
+    bool finish[P];
+    for(int i=0;i<n;i++)
+    	finish[i]=0; 
+  
+    // To store safe sequence 
     int safeSeq[P]; 
   
-
+    // Make a copy of available resources 
     int work[R]; 
     for (int i = 0; i < R ; i++) 
         work[i] = avail[i]; 
-
-
+  
+    // While all processes are not finished 
+    // or system is not in safe state. 
     int count = 0; 
     while (count < P) 
     { 
-    
-    
-    
+        // Find a process which is not finish and 
+        // whose needs can be satisfied with current 
+        // work[] resources. 
         bool found = false; 
         for (int p = 0; p < P; p++) 
         { 
-        
-        
+            // First check if a process is finished, 
+            // if no, go for next condition 
             if (finish[p] == 0) 
             { 
-            
-            
-            
+                // Check if for all resources of 
+                // current P need is less 
+                // than work 
                 int j; 
                 for (j = 0; j < R; j++) 
                     if (need[p][j] > work[j]) 
                         break; 
   
-            
+                // If all needs of p were satisfied. 
                 if (j == R) 
                 { 
-                
-                
-                
+                    // Add the allocated resources of 
+                    // current P to the available/work 
+                    // resources i.e.free the resources 
                     for (int k = 0 ; k < R ; k++) 
                         work[k] += allot[p][k]; 
   
-                
+                    // Add this process to safe sequence. 
                     safeSeq[count++] = p; 
   
-                
+                    // Mark this p as finished 
                     finish[p] = 1; 
   
                     found = true; 
@@ -75,8 +113,8 @@ bool isSafe(int processes[], int avail[], int maxm[][R],
             } 
         } 
   
-    
-    
+        // If we could not find a next process in safe 
+        // sequence. 
         if (found == false) 
         { 
             cout << "System is not in safe state"; 
@@ -84,8 +122,8 @@ bool isSafe(int processes[], int avail[], int maxm[][R],
         } 
     } 
   
-
-
+    // If system is in safe state then 
+    // safe sequence will be as below 
     cout << "System is in safe state.\nSafe"
          " sequence is: "; 
     for (int i = 0; i < P ; i++) 
@@ -93,28 +131,46 @@ bool isSafe(int processes[], int avail[], int maxm[][R],
   
     return true; 
 } 
-  
-int main() 
-{ 
 
-    int processes[] = {0, 1, 2}; 
-  
+int main()
+{
+	int n,m;
+	cin>>n>>m;
 
-    int avail[] = {12}; 
-  
+	int *availiable = new int[m];
 
+	int **max = new int*[n];
+	for(int i=0;i<n;i++) max[i]=new int[m];
 
-    int maxm[][R] = {{10}, 
-                     {4}, 
-                     {9}}; 
-  
+	int **allocation = new int*[n];
+	for(int i=0;i<n;i++) allocation[i]=new int[m];
 
-    int allot[][R] = {{5}, 
-                      {2}, 
-                      {2}}; 
-  
+	int **req = new int*[n];
+	for(int i=0;i<n;i++) req[i]=new int[m];
 
-    isSafe(processes, avail, maxm, allot); 
-    cout<<endl;
-    return 0; 
+	cout<<"Enter availiablity matrix\n";
+	for(int i=0;i<m;i++)
+		cin>>availiable[i];
+
+	cout<<"Enter max matrix\n";
+	for(int i=0;i<n;i++)
+		for(int j=0;j<m;j++)
+			cin>>max[i][j];
+
+	cout<<"Enter allocation matrix\n";
+	for(int i=0;i<n;i++)
+		for(int j=0;j<m;j++)
+			cin>>allocation[i][j];
+
+	cout<<"Enter request matrix?\n";
+	for(int i=0;i<n;i++)
+		for(int j=0;j<m;j++)
+			cin>>req[i][j];	
+
+	isSafe(availiable, max, allocation, n, m);
+	request(availiable, max, allocation, req, n, m);
+	isSafe(availiable, max, allocation, n, m);
+
+	return 0;
+
 }
